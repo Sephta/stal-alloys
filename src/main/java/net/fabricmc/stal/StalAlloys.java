dcbc32af91b2130228c38f91f305897caad9180b
Seth Tal <Seth.tal255@gmail.com>
2023-02-26 08:18:51 -0800

    Switched mod entry point
    switch mod entrypoint from 'example' to 'stal'

--- src/main/java/net/fabricmc/stal/StalAlloys.java
+++ src/main/java/net/fabricmc/stal/StalAlloys.java
@@ -1,14 +1,25 @@
-package net.fabricmc.example;
+package net.fabricmc.stal;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.registry.*;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class StalAlloys implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	public static final  Item STEEL_INGOT = Registry.register(
+		Registries.ITEM, 
+		new Identifier("stal", "steel_ingot"), 
+		new Item(new FabricItemSettings())
+	);
 
 	@Override
 	public void onInitialize() {
@@ -16,6 +27,7 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello World - STAL ALLOYS!");
+
 	}
 }
