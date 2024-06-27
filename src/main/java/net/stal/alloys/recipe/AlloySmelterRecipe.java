aefa93d636cef174f5e994f32c2ab5e55f8e94d8
Seth Tal <Seth.tal255@gmail.com>
2024-06-26 21:38:12 -0700

    initial REI integration
    - setup REI such that recipes actually get displayed correctly. Needs more work

--- src/main/java/net/stal/alloys/recipe/AlloySmelterRecipe.java
+++ src/main/java/net/stal/alloys/recipe/AlloySmelterRecipe.java
@@ -70,6 +70,20 @@ public class AlloySmelterRecipe implements Recipe<SimpleInventory> {
   }
 
   @Override
+  public DefaultedList<Ingredient> getIngredients() {
+    DefaultedList<Ingredient> list = DefaultedList.ofSize(this.mRecipeItems.size());
+    list.addAll(mRecipeItems);
+    return list;
+  }
+
+  @Override
+  public DefaultedList<Ingredient> getIngredients() {
+    DefaultedList<Ingredient> list = DefaultedList.ofSize(this.mRecipeItems.size());
+    list.addAll(mRecipeItems);
+    return list;
+  }
+
+  @Override
   public ItemStack getOutput(DynamicRegistryManager dynamicRegistryManager) {
     return mOutput.copy();
   }
